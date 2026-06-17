3cc2251288fe8522d5f71d713cee9b7e2b59ecf1
sayyedmujib11m <149274455+sayyedmujib11m@users.noreply.github.com>
2026-06-18 01:54:40 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "app": "FreezeFont",
+        "status": "running"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
