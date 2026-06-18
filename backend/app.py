034cd2f4039658778c6152791bb1f37f0b36eafa
sayyedmujib11m <sayyedmujib09@gmail.com>
2026-06-18 13:02:55 +0530

    Working font conversion backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from backend.freeze_font import freeze_font
+from pathlib import Path
+import shutil
+import os
 
 app = FastAPI()
+
+UPLOAD_FOLDER = "uploads"
+OUTPUT_FOLDER = "output"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
@@ -9,8 +19,20 @@ def home():
         "status": "running"
     }
 
-@app.get("/hello")
-def hello():
+@app.post("/upload")
+async def upload_font(file: UploadFile = File(...)):
+    upload_path = Path(UPLOAD_FOLDER) / file.filename
+
+    with open(upload_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    zip_path = freeze_font(
+        str(upload_path),
+        OUTPUT_FOLDER
+    )
+
     return {
-        "message": "FreezeFont API is working"
+        "success": True,
+        "filename": file.filename,
+        "zip_file": zip_path
     }
